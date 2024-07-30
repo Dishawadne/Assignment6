bc2d929dfe9e70ebe3dc6424ba223fb450da29c1
Dishawadne <wadnedisha776@gmail.com>
2024-07-30 15:02:17 +0530

    Add project files.

---
+++ Assignment6/Pizza.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6
+{
+    internal class Pizza
+    {
+
+        private String PizzaSize { get; set; }
+        private int CheeseToppings {  get; set; }
+
+        private int PepperoniToppings {  get; set; }
+        private int HamToppings {  get; set; }
+
+
+        public Pizza(string pizzaSize, int cheeseToppings, int pepperoniToppings, int hamToppings)
+        {
+            this.PizzaSize = pizzaSize;
+            this.CheeseToppings = cheeseToppings;
+            this.PepperoniToppings = pepperoniToppings;
+            this.HamToppings = hamToppings;
+        }
+
+        public double calcCost()
+        {
+            double cost = 0;
+            switch (PizzaSize.ToLower())
+            {
+                case "small":
+                    {
+                        cost = 10;
+                        break;
+                    }
+                case "medium":
+                    {
+                        cost = 12;
+                        break;
+                    }
+                case "large":
+                    {
+                        cost = 14;
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Invalid pizza size");
+                        break;
+                    }
+            }
+                 return cost + 2 * (CheeseToppings + PepperoniToppings + HamToppings);
+            
+        }
+        public string getDescription()
+        {
+            return $"Size: {PizzaSize}, Cheese Toppings: {CheeseToppings}, Pepperoni Toppings: {PepperoniToppings}, Ham Toppings: {HamToppings}";
+        }
+
+
+
+
+    }
+}
---
+++ Assignment6/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            List<Pizza> pizzas = new List<Pizza>
+        {
+            new Pizza("small", 2, 2, 1),
+            new Pizza("medium", 3, 1, 2),
+            new Pizza("large", 2, 1, 1)
+        };
+
+            foreach (Pizza pizza in pizzas)
+            {
+                Console.WriteLine(pizza.getDescription());
+                Console.WriteLine($"Cost: ${pizza.calcCost()}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
